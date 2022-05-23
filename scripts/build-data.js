3a85bf12f78b176fa97a6c3232c3af189c9561e6
isaced <isaced@163.com>
2022-05-23 21:41:06 +0800

    Add scripts to fetch github repo info

---
+++ scripts/build-data.js
@@ -0,0 +1,242 @@
+import fs from "fs";
+import jsonfile from "jsonfile";
+import chunk from "lodash/chunk";
+import path from "path";
+
+import debugGithubRepos from "../debug-github-repos.json";
+import githubRepos from "../cocoa-libraries.json";
+import * as Strings from "../util/strings";
+// import { calculateDirectoryScore, calculatePopularityScore } from './calculate-score';
+import { fetchGithubData, fetchGithubRateLimit, loadGitHubLicenses } from "./fetch-github-data";
+// import { fetchNpmData, fetchNpmDataBulk } from "./fetch-npm-data";
+// import fetchReadmeImages from './fetch-readme-images';
+
+// Uses debug-github-repos.json instead, so we have less repositories to crunch
+// each time we run the script
+const USE_DEBUG_REPOS = false;
+
+// Loads the Github API results from disk rather than hitting the API each time.
+// The first run will hit the API if raw-github-results.json doesn't exist yet.
+const LOAD_GITHUB_RESULTS_FROM_DISK = false;
+const GITHUB_RESULTS_PATH = path.join("scripts", "raw-github-results.json");
+
+// If script should try to scrape images from GitHub repositories.
+const SCRAPE_GH_IMAGES = true;
+
+const JSON_OPTIONS = {
+  spaces: 2,
+};
+
+export const sleep = (ms = 0, msMax = null) => {
+  return new Promise((r) => setTimeout(r, msMax ? Math.floor(Math.random() * (msMax - ms)) + ms : ms));
+};
+
+const fillNpmName = (project) => {
+  if (!project.npmPkg) {
+    const parts = project.githubUrl.split("/");
+    project.npmPkg = parts[parts.length - 1].toLowerCase();
+  }
+  return project;
+};
+
+const invalidRepos = [];
+
+const buildAndScoreData = async () => {
+  console.log("** Loading data from GitHub");
+  let data = await loadRepositoryDataAsync();
+
+  data = data.filter((project) => {
+    if (!project.github || Strings.isEmptyOrNull(project.github.name)) {
+      invalidRepos.push(project.githubUrl);
+      return false;
+    }
+    return !Strings.isEmptyOrNull(project.github.name);
+  });
+
+  // if (SCRAPE_GH_IMAGES) {
+  //   console.log("\n** Scraping images from README");
+  //   await sleep(1000);
+  //   data = await Promise.all(data.map((project) => fetchReadmeImages(project)));
+  // }
+
+  // console.log("\n** Determining npm package name");
+  // await sleep(1000);
+  // data = data.map(fillNpmName);
+
+  // console.log("\n** Loading download stats from npm");
+  // await sleep(1000);
+
+  // https://github.com/npm/registry/blob/master/docs/download-counts.md#bulk-queries
+  // let bulkList = [];
+
+  // // Fetch scope packages data
+  // data = await Promise.all(
+  //   data.map((project) => {
+  //     if (project.npmPkg.startsWith("@")) {
+  //       return fetchNpmData(project);
+  //     } else {
+  //       bulkList.push(project.npmPkg);
+  //       return project;
+  //     }
+  //   })
+  // );
+
+  // Assemble and fetch data in bulk queries
+  // const CHUNK_SIZE = 32;
+  // bulkList = [...Array(Math.ceil(bulkList.length / CHUNK_SIZE))].map((_) => bulkList.splice(0, CHUNK_SIZE));
+
+  // const downloadsList = (await Promise.all(bulkList.map((chunk) => fetchNpmDataBulk(chunk)))).flat();
+  // const downloadsListWeek = (await Promise.all(bulkList.map((chunk) => fetchNpmDataBulk(chunk, "week")))).flat();
+
+  // // Fill npm data from bulk queries
+  // data = data.map((project) =>
+  //   project.npm
+  //     ? project
+  //     : {
+  //         ...project,
+  //         npm: {
+  //           ...(downloadsList.find((d) => d.name === project.npmPkg)?.npm || {}),
+  //           ...(downloadsListWeek.find((d) => d.name === project.npmPkg)?.npm || {}),
+  //         },
+  //       }
+  // );
+
+  // console.log("\n** Calculating Directory Score");
+  // data = data.map((project) => {
+  //   try {
+  //     return calculateDirectoryScore(project);
+  //   } catch (e) {
+  //     console.log(`Failed to calculate score for ${project.github.name}`, e.message);
+  //   }
+  // });
+
+  // console.log("\n** Calculating popularity");
+  // data = data.map((project) => {
+  //   try {
+  //     return calculatePopularityScore(project);
+  //   } catch (e) {
+  //     console.log(`Failed to calculate popularity for ${project.github.name}`, e.message);
+  //     console.log(project.githubUrl);
+  //   }
+  // });
+
+  // Process topic counts
+  const topicCounts = {};
+  data.forEach((project, index, projectList) => {
+    let topicSearchString = "";
+
+    if (project.github.topics) {
+      project.github.topics.forEach((topic) => {
+        topicSearchString = `${topicSearchString} ${topic}`;
+
+        if (!topicCounts[topic]) {
+          topicCounts[topic] = 1;
+          return;
+        }
+
+        topicCounts[topic] += 1;
+      });
+    }
+
+    projectList[index].topicSearchString = topicSearchString.trim();
+  });
+
+  if (invalidRepos.length) {
+    console.log(
+      "** The following repositories were unable to fetch from GitHub, they may need to be removed from react-native-libraries.json:"
+    );
+    invalidRepos.forEach((repoUrl) => console.log(`- ${repoUrl}`));
+  }
+
+  return jsonfile.writeFile(
+    path.resolve("assets", "data.json"),
+    {
+      libraries: data,
+      topics: topicCounts,
+      topicsList: Object.keys(topicCounts).sort(),
+    },
+    JSON_OPTIONS,
+    (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("\n** Done!");
+      }
+    }
+  );
+};
+
+async function fetchGithubDataThrottled({ data, chunkSize, staggerMs }) {
+  let results = [];
+  const chunks = chunk(data, chunkSize);
+  for (const c of chunks) {
+    if (chunks.indexOf(c) > 0) {
+      console.log(`${results.length} of ${data.length} fetched`);
+      console.log(`Sleeping ${staggerMs}ms`);
+      await sleep(staggerMs);
+    }
+
+    const partialResult = await Promise.all(c.map(fetchGithubData));
+    results = [...results, ...partialResult];
+
+    if (partialResult.length !== c.length) {
+      throw new Error(
+        `Error in fetching data from GitHub... Expected ${c.length} results but only received ${partialResult.length}`
+      );
+    }
+  }
+
+  return results;
+}
+
+async function loadRepositoryDataAsync() {
+  const data = USE_DEBUG_REPOS ? debugGithubRepos : githubRepos;
+  let githubResultsFileExists = false;
+  try {
+    fs.statSync(GITHUB_RESULTS_PATH);
+    githubResultsFileExists = true;
+  } catch (e) {}
+
+  const { apiLimit, apiLimitRemaining, apiLimitCost } = await fetchGithubRateLimit();
+
+  // 5000 requests per hour is the authenticated API request rate limit
+  if (!apiLimit || apiLimit < 5000) {
+    throw new Error("GitHub API token is invalid or query is not properly configured.");
+  }
+
+  // Error out if not enough remaining
+  if (apiLimitRemaining < data.length * apiLimitCost) {
+    throw new Error("Not enough requests left on GitHub API rate limiting to proceed.");
+  }
+
+  console.info(
+    `${apiLimitRemaining} of ${apiLimit} GitHub API requests remaining for the hour at a cost of ${apiLimitCost} per request`
+  );
+
+  await loadGitHubLicenses();
+
+  let result;
+  if (LOAD_GITHUB_RESULTS_FROM_DISK && githubResultsFileExists) {
+    result = jsonfile.readFileSync(GITHUB_RESULTS_PATH);
+    console.log("Loaded Github results from disk, skipped API calls");
+  } else {
+    result = await fetchGithubDataThrottled({ data, chunkSize: 25, staggerMs: 5000 });
+
+    if (LOAD_GITHUB_RESULTS_FROM_DISK) {
+      await new Promise((resolve, reject) => {
+        jsonfile.writeFile(GITHUB_RESULTS_PATH, result, JSON_OPTIONS, (err) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve();
+          }
+        });
+      });
+      console.log("Saved Github results from disk");
+    }
+  }
+
+  return result;
+}
+
+buildAndScoreData();
---
+++ scripts/fetch-github-data.js
@@ -0,0 +1,308 @@
+import fetch from "cross-fetch";
+
+import { sleep } from "./build-data";
+
+require("dotenv").config();
+
+const GRAPHQL_API = "https://api.github.com/graphql";
+
+const Authorization = `bearer ${process.env.GITHUB_TOKEN}`;
+
+const licenses = {};
+
+/**
+ * Fetch licenses from github to be used later to parse licenses from npm
+ */
+export const loadGitHubLicenses = async () => {
+  const query = `
+    query {
+      licenses {
+        name
+        url
+        id
+        key
+        spdxId
+      }
+    }
+  `;
+
+  const result = await makeGraphqlQuery(query);
+
+  result.data.licenses.forEach((license) => {
+    licenses[license.key] = license;
+  });
+};
+
+const query = `
+  query ($repoOwner: String!, $repoName: String!, $packagePath: String = ".", $packageJsonPath: String = "HEAD:package.json") {
+    rateLimit {
+      limit
+      cost
+      remaining
+      resetAt
+    }
+    repository(owner: $repoOwner, name: $repoName) {
+      hasIssuesEnabled
+      hasWikiEnabled
+      issues(states: OPEN) {
+        totalCount
+      }
+      watchers {
+        totalCount
+      }
+      stargazers {
+        totalCount
+      }
+      forks {
+        totalCount
+      }
+      description
+      createdAt
+      pushedAt
+      updatedAt
+      homepageUrl
+      url
+      mirrorUrl
+      name
+      nameWithOwner
+      isArchived
+      isMirror
+      licenseInfo {
+        key
+        name
+        spdxId
+        url
+        id
+      }
+      deployments {
+        totalCount
+      }
+      releases(first: 1, orderBy: {field: CREATED_AT, direction: DESC}) {
+        nodes {
+          name
+          tagName
+          createdAt
+          publishedAt
+          isPrerelease
+        }
+      }
+      repositoryTopics(first: 10) {
+        nodes {
+          topic {
+            name
+          }
+        }
+      }
+      defaultBranchRef {
+        target {
+          ... on Commit {
+            id
+            history(path: $packagePath, first: 1) {
+              nodes {
+                committedDate
+                message
+              }
+            }
+          }
+        }
+      }
+      packageJson:object(expression: $packageJsonPath) {
+        ... on Blob {
+          text
+        }
+      }
+    }
+  }
+`;
+
+const makeGraphqlQuery = async (query, variables) => {
+  const result = await fetch(GRAPHQL_API, {
+    method: "POST",
+    headers: {
+      Authorization,
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  });
+  return await result.json();
+};
+
+export const fetchGithubRateLimit = async () => {
+  // Accurately fetch query rate limit and cost by making dummy request
+  // https://developer.github.com/v4/guides/resource-limitations/
+  const result = await makeGraphqlQuery(query, {
+    repoOwner: "react-native-directory",
+    repoName: "website",
+  });
+
+  if (result.data) {
+    const { rateLimit } = result.data;
+    return {
+      apiLimit: rateLimit.limit,
+      apiLimitRemaining: rateLimit.remaining,
+      apiLimitCost: rateLimit.cost,
+    };
+  }
+
+  if (result.errors) {
+    console.log("[GH] GraphQL API error:", result.errors);
+  }
+
+  return {};
+};
+
+const getUpdatedUrl = async (url) => {
+  try {
+    const result = await fetch(url);
+    return result.url;
+  } catch {
+    return url;
+  }
+};
+
+const parseUrl = (url) => {
+  const [, , , repoOwner, repoName, ...path] = url.split("/");
+  const isMonorepo = !!(path && path.length);
+  const packagePath = isMonorepo ? path.slice(2).join("/") : ".";
+
+  return {
+    repoOwner,
+    repoName,
+    isMonorepo,
+    packagePath,
+  };
+};
+
+export const fetchGithubData = async (data, retries = 2) => {
+  if (retries < 0) {
+    console.warn(`[GH] ERROR fetching ${data.githubUrl} - OUT OF RETRIES`);
+    return data;
+  }
+  try {
+    const url = data.githubUrl;
+    const { isMonorepo, repoOwner, repoName, packagePath } = parseUrl(url);
+
+    const result = await makeGraphqlQuery(query, {
+      repoOwner,
+      repoName,
+      packagePath,
+      packageJsonPath: `HEAD:${packagePath === "." ? "" : `${packagePath}/`}package.json`,
+    });
+
+    if ((!result.data || !result.data.repository) && result.errors) {
+      if (result.errors[0].type === "NOT_FOUND") {
+        const newUrl = await getUpdatedUrl(url);
+        if (newUrl !== url) {
+          console.warn(`[GH] Repository ${repoOwner}/${repoName} has moved to ${newUrl}`);
+          data.githubUrl = newUrl;
+        } else {
+          console.warn(`[GH] Repository ${repoOwner}/${repoName} not found`);
+        }
+      } else {
+        console.warn(`[GH] Data fetch error for ${repoOwner}/${repoName}`, result.errors);
+      }
+
+      console.log(`[GH] Retrying fetch for ${data.githubUrl}`);
+      await sleep(2500);
+      return await fetchGithubData(data, retries - 1);
+    }
+
+    const github = createRepoDataWithResponse(result.data.repository, isMonorepo);
+    return {
+      ...data,
+      github,
+    };
+  } catch (e) {
+    console.log(`[GH] Retrying fetch for ${data.githubUrl}`, e);
+    await sleep(2500);
+    return await fetchGithubData(data, retries - 1);
+  }
+};
+
+const getLicenseFromPackageJson = (packageJson) => {
+  // Get the GitHub license spec from the npm string
+  if (packageJson.license && typeof packageJson.license === "string") {
+    return licenses[packageJson.license.toLowerCase()];
+  }
+};
+
+const processTopics = (topics) => (topics || []).map((topic) => topic.replace(/([ _])/g, "-").toLowerCase());
+
+const createRepoDataWithResponse = (json, monorepo) => {
+  if (json.packageJson) {
+    try {
+      const packageJson = JSON.parse(json.packageJson.text);
+
+      if (monorepo) {
+        json.homepageUrl = packageJson.homepage;
+        json.name = packageJson.name;
+        json.topics = processTopics(packageJson.keywords);
+        json.description = packageJson.description;
+        json.licenseInfo = getLicenseFromPackageJson(packageJson);
+      }
+
+      if (!monorepo) {
+        json.topics = [
+          ...new Set([
+            ...processTopics(packageJson.keywords),
+            ...processTopics(json.repositoryTopics.nodes.map(({ topic }) => topic.name)),
+          ]),
+        ];
+
+        if (!json.description) {
+          json.description = packageJson.description;
+        }
+
+        if (!json.licenseInfo || (json.licenseInfo && json.licenseInfo.key === "other")) {
+          json.licenseInfo = getLicenseFromPackageJson(packageJson) || json.licenseInfo;
+        }
+      }
+
+      if (packageJson.types || packageJson.typings) {
+        json.types = true;
+      }
+    } catch (e) {
+      console.warn(`Unable to parse ${json.name} package.json file!`);
+      console.error(e);
+    }
+  }
+
+  if (!monorepo) {
+    json.lastRelease =
+      json.releases && json.releases.nodes && json.releases.nodes.length ? json.releases.nodes[0] : undefined;
+  }
+
+  const lastCommitAt = json.defaultBranchRef.target.history.nodes[0].committedDate;
+
+  return {
+    urls: {
+      repo: json.url,
+      clone: `${json.url}.git`,
+      homepage: json.homepageUrl,
+    },
+    stats: {
+      hasIssues: json.hasIssuesEnabled,
+      hasWiki: json.hasWikiEnabled,
+      hasPages: json.deployments.totalCount > 0,
+      hasDownloads: true,
+      hasTopics: json.topics && json.topics.length > 0,
+      updatedAt: lastCommitAt,
+      createdAt: json.createdAt,
+      pushedAt: lastCommitAt,
+      forks: json.forks.totalCount,
+      issues: json.issues.totalCount,
+      subscribers: json.watchers.totalCount,
+      stars: json.stargazers.totalCount,
+    },
+    name: json.name,
+    fullName: json.nameWithOwner,
+    description: json.description,
+    topics: json.topics,
+    license: json.licenseInfo,
+    lastRelease: json.lastRelease,
+    hasTypes: json.types,
+  };
+};
